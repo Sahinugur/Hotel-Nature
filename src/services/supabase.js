39bd4cefa25d936203e5b7197459405abb8e0123
Sahinugur <ugurlao@gmail.com>
2023-08-25 16:24:38 +0200

    Editing of Cabin

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://igbhsojwaifoskyoyhry.supabase.co";
+export const supabaseUrl = "https://igbhsojwaifoskyoyhry.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlnYmhzb2p3YWlmb3NreW95aHJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI2MTM4NTUsImV4cCI6MjAwODE4OTg1NX0.xhXo2YdQQyHjrQQHRhiIyvk8Zyd6Btq9RZi6Q-R3OUI";
 const supabase = createClient(supabaseUrl, supabaseKey);
