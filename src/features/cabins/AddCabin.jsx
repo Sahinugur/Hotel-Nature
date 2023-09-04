53ce396b8edc41508b36489bf16d61f0ef9712ee
Sahinugur <ugurlao@gmail.com>
2023-09-04 15:00:48 +0200

    Compound Component Model implemented in diffrent places

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,9 +1,23 @@
 import Button from "../../ui/Button";
-import { useState } from "react";
 import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
 
 export default function AddCabin() {
+  return (
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add Cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+    </div>
+  );
+}
+
+/* export default function AddCabin() {
   const [isOpenModel, setIsOpenModel] = useState(false);
   return (
     <div>
@@ -21,4 +35,4 @@ export default function AddCabin() {
       )}
     </div>
   );
-}
+} */
---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,21 @@
+import { useRef, useEffect } from "react";
+
+export default function useOutsideClick(handler) {
+  const ref = useRef();
+  useEffect(
+    function () {
+      function handleClick(e) {
+        if (ref.current && !ref.current.contains(e.target)) {
+          handler();
+        }
+      }
+
+      document.addEventListener("click", handleClick, true);
+
+      return () => document.removeEventListener("click", handleClick, true);
+    },
+    [handler]
+  );
+
+  return ref;
+}
